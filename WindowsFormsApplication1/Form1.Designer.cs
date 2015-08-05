5f0b79ba9b5e7c027b01d7f4b07245be924ffd1e
Mada Reda <mada.reda.w@gmail.com>
2015-08-05 15:37:54 +0200

    Rename
    Rename and list...

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -31,7 +31,9 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.startbtn = new System.Windows.Forms.Button();
             this.pathtxt = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+           
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // startbtn
@@ -51,28 +53,39 @@
             this.pathtxt.Size = new System.Drawing.Size(250, 20);
             this.pathtxt.TabIndex = 1;
             // 
-            // label1
+           
+            // checkBox1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 27);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(13, 119);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(65, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Rename";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter The Path";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox1);
+            //this.Controls.Add(this.label1);
             this.Controls.Add(this.pathtxt);
             this.Controls.Add(this.startbtn);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Files Lister";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,7 +95,9 @@
 
         private System.Windows.Forms.Button startbtn;
         private System.Windows.Forms.TextBox pathtxt;
-        private System.Windows.Forms.Label label1;
+        
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -16,7 +16,8 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
-        string path,filepath;
+        string path, filepath;
+        int index = 0;
         public Form1()
         {
             InitializeComponent();
@@ -24,12 +25,37 @@ namespace WindowsFormsApplication1
 
         private void startbtn_Click(object sender, EventArgs e)
         {
+            index = 0;
             path = pathtxt.Text;
             filepath = path + "\\files.txt";
-            MessageBox.Show(filepath);
+          //  MessageBox.Show(filepath);
 
             try
             {
+                //Rename
+                if (checkBox1.CheckState.ToString() == "Checked")
+                {
+                   // MessageBox.Show(checkBox1.CheckState.ToString());
+                    //Get files
+                    string strindex, newpath;
+                    foreach (var npath in Directory.GetFiles(path))
+                    {
+                        if(index < 10){
+
+                            strindex = "0"+index.ToString();
+                        }
+                        else
+                        {
+                            strindex = index.ToString();
+                        }
+                        
+                        newpath = path + "\\" + strindex + ".jpg";
+                        System.IO.File.Delete(newpath);
+                        System.IO.File.Move(npath, newpath);
+                        index++;
+                        Console.WriteLine(path + strindex);
+                    }
+                }
 
                 // Delete the file if it exists. 
                 if (File.Exists(filepath))
@@ -52,12 +78,12 @@ namespace WindowsFormsApplication1
                         Console.WriteLine(npath); // full path
                         Console.WriteLine(System.IO.Path.GetFileName(npath)); // file name
                         // Add some information to the file.
-                        fs.WriteLine(npath);
+                        fs.WriteLine("'" + npath + "',");
                     }
 
+                    MessageBox.Show("Completed");
 
-                    
-                    
+
                 }
             }
             catch (Exception ex)
